5940419d12cf29501eca5fc450a0f0f2ed51e26e
murtrag <lukasz.czernik@gmail.com>
2020-07-12 15:36:23 +0200

    ff

--- mixer.py
+++ mixer.py
@@ -2,39 +2,42 @@ from status import Status_Bar
 from pyclick import HumanClicker # super symulacja ruchu myszki
 import pyautogui
 import time
-from random import randint
+from random import randint, random
 import find_in_box
 mouse = HumanClicker()
 
-inventory = find_in_box.FindItem('media/box-titles/inventory.png')
 while True:
-	try:
-		inventory = find_in_box.FindItem('media/box-titles/inventory.png')
-		inv_cord = inventory.get_box_dimention()
-		mouse.move((randint(inv_cord[0]+34, inv_cord[0]+124),randint(inv_cord[1]+30,inv_cord[1]+38)),2 )
-		pyautogui.doubleClick()  #dbl click first herb
+        try:
+            inventory = find_in_box.FindItem('media/box-titles/backpack.png')
+            inv_cord = inventory.get_box_dimention()
+            print('1')
+            x, y = (randint(inv_cord[0]+34, inv_cord[0]+124),randint(inv_cord[1]+20,inv_cord[1]+48))
+            mouse.move((x,y),2 )
+            pyautogui.doubleClick()  #dbl click first herb
 
-		mouse.move((randint(inv_cord[0]+34, inv_cord[0]+124),randint(inv_cord[1]+45,inv_cord[1]+54)),2 )
-		pyautogui.rightClick()  #open menu with mixing option
-		time.sleep(randint(1,2))
-		try:
-			alchemy_raw = pyautogui.locateOnScreen('media/menu/alchemy.png')  #find alchemy option
-			assert alchem_raw
-		except:			
-			alchemy_raw = pyautogui.locateOnScreen('media/menu/alchemy3.png')  #find alchemy option
-		finally:
-			alchemy_x, alchemy_y = pyautogui.center(alchemy_raw)
+            mouse.move((x+randint(1,4),y+randint(30,150)),1 )
+            pyautogui.rightClick()  #open menu with mixing option
+            print('2')
+            time.sleep(randint(1,2))
+            print('3')
+            alchemy_raw = pyautogui.locateOnScreen('media/menu/alchemy.png', confidence=.8)  #find alchemy option
+            alchemy_x, alchemy_y = pyautogui.center(alchemy_raw)
+            print('4')
 
 
-		mouse.move((randint(alchemy_x-3, alchemy_x+3),randint(alchemy_y-3,alchemy_y+3)),2 )  #mouse on alchemy position
-		time.sleep(randint(1,2))
+            mouse.move((randint(alchemy_x-3, alchemy_x+3),randint(alchemy_y-3,alchemy_y+3)),2 )  #mouse on alchemy position
+            print('5')
+            time.sleep(randint(1,2))
+            print('6')
 
-		mix_raw = pyautogui.locateOnScreen('media/menu/mix.png')  #find mix option
-		mix_x, mix_y = pyautogui.center(mix_raw)
-		pyautogui.moveTo(randint(mix_x-3, mix_x+3),randint(mix_y-3,mix_y+3), randint(1,3), pyautogui.easeInQuad)
-		pyautogui.click()
+            mix_raw = pyautogui.locateOnScreen('media/menu/mix.png', confidence=.8)  #find mix option
+            mix_x, mix_y = pyautogui.center(mix_raw)
+            pyautogui.moveTo(randint(mix_x-3, mix_x+3),randint(mix_y-3,mix_y+3), random(), pyautogui.easeInQuad)
+            # pyautogui.moveTo(randint(mix_x-3, mix_x+3),randint(mix_y-3,mix_y+3), random.random(), pyautogui.easeInQuad)
+            pyautogui.click()
+            print('7')
 
-		time.sleep(randint(15,17))
-	except:
-		pyautogui.moveTo(inv_cord[0],inv_cord[1],1, pyautogui.easeInQuad)
-		pyautogui.click()
+            time.sleep(randint(2,8))
+        except:
+            pyautogui.moveTo(inv_cord[0],inv_cord[1],0.5, pyautogui.easeInQuad)
+            pyautogui.click()
